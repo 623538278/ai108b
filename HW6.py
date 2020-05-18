a9aff5eca49c139ccbd34b0b32e0abf27f69de2a
623538278 <40665602+623538278@users.noreply.github.com>
2020-05-18 20:05:13 +0800

    Update HW6.py

--- HW6.py
+++ HW6.py
@@ -1,1 +1,36 @@
+import random as r
 
+'''
+
+S = NP VP
+NP = DET N
+VP = V NP
+N = dog | cat
+V = chase | eat
+DET = a | the
+
+'''
+
+A=['张三', '李四', '王五' , '德萊厄斯']
+B=['想', '不想']
+C=['在你嘴裡种水稻', '請你吃飯', '外圈刮']
+
+def S():
+
+    return N() + V() + DET()
+
+def N():
+
+    return r.choice(A)
+
+def V():
+
+    return r.choice(B)
+
+def DET():
+
+    return r.choice(C)
+
+for a in range(3):
+
+    print(S())
